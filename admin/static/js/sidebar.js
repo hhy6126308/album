a188755a6b582f84da2a153a313bd45a46aafaf3
huanghaiyan <hhy6216308@163.com>
2018-07-10 02:12:33 +0800

    album  init5

---
+++ admin/static/js/sidebar.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('ul li a').bind("click",function(){
+		$('ul > li > a').each(function(i){
+			$(this).removeClass("active");
+		});
+		this.blur();
+		this.className = "active";
+	});
+	$('#sidebar .con h2 span').bind("click",function(){
+		if(this.className == "close"){
+			this.className = "open";
+			$(this.parentNode.parentNode).find("ul").hide()
+		}else{
+			this.className = "close";
+			$(this.parentNode.parentNode).find("ul").show()
+		}
+	
+	});
+	$('#sidebar .item').each(function(){
+		var l	= $(this).find('li').length;
+		if(l <=0){
+			$(this).hide();
+		}
+	});
+});
+
