9828832bb29ae29eb2473ba13aa1aea7d208632b
黄海燕 <hhy6126308@163.com>
2018-08-26 20:04:27 +0800

    upload bug

--- api/Home/Controller/UploadController.class.php
+++ api/Home/Controller/UploadController.class.php
@@ -17,7 +17,7 @@ class UploadController extends BaseController
             $upload            = new \Vendor\UploadFile();// 实例化上传类
             $upload->maxSize   = 3145728;// 设置附件上传大小
             $upload->allowExts = ['jpg', 'gif', 'png', 'jpeg'];// 设置附件上传类型
-            $upload->savePath  = C('SavePicPath');// 设置附件上传目录
+            $upload->savePath  = C('SavePicPath') . '/';// 设置附件上传目录
             if (!$upload->upload()) {// 上传错误提示错误信息
                 $rs['msg'] = $upload->getErrorMsg();
             } else {// 上传成功 获取上传文件信息
