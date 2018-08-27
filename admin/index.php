22dec2031937e1845cf20f3fced7924b30dcc5be
zengxiangming <zengxiangming@touguyun.cn>
2018-08-28 00:48:20 +0800

    update

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 //local
-$_SERVER['PATH_INFO'] = parse_url($_SERVER['REQUEST_URI'])['path'];
+//$_SERVER['PATH_INFO'] = parse_url($_SERVER['REQUEST_URI'])['path'];
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',true);
 
