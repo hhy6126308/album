90f5fef6d1868cdec0996aad3733b6a367d99777
黄海燕 <hhy6126308@163.com>
2018-08-31 02:21:55 +0800

    人脸识别

--- api/Home/Controller/AlbumSettingController.class.php
+++ api/Home/Controller/AlbumSettingController.class.php
@@ -1,6 +1,7 @@
 <?php
 namespace Home\Controller;
 
+use Home\Model\AlbumModel;
 use \Home\Model\AlbumSettingModel;
 
 class AlbumSettingController extends BaseController {
@@ -14,6 +15,15 @@ class AlbumSettingController extends BaseController {
         $info = $album->where("id=$uid")->field('reward')->find();
         if(!empty($info)){
             $rs['data']['reward'] = $info['reward'];
+        }
+
+        $album_id = safe_string($_GET['album_id']);
+        if ($album_id) {
+            $albumM = new AlbumModel();
+            $album = $albumM->where("id = $album_id")->find();
+            if ($album) {
+                $rs['data']['reward'] = $rs['data']['reward'] && $album['is_reward'] ? 1 : 0;
+            }
         }
         
         $this->out_put($rs);
